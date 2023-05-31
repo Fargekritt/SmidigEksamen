8a53981cbc8a6a4bf9988a8fb2623df0473fc1af
Amund <lullinj98@gmail.com>
2023-05-31 12:46:43 +0200

    Added basic spring boot setup

---
+++ backend/src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,3 @@
+package com.example.demo;
+
+public record Greeting(long id, String content) { }
---
+++ backend/src/main/java/com/example/demo/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+
+@RestController
+@RequestMapping("/api")
+public class GreetingController {
+
+    private static final String template = "Hello %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "world") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+    @GetMapping("/ost")
+    public String oste(){
+        return "chees";
+    }
+}
