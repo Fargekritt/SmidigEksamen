5ad5d77c4fe01cee43dbd15c2d737618fedb1bed
RikaTob <rikatob@hotmail.com>
2023-06-01 11:46:33 +0200

    Journey DTO * Created fields(example). * Created getters and setters.

--- backend/src/main/java/com/example/demo/dto/JourneyDto.java
+++ backend/src/main/java/com/example/demo/dto/JourneyDto.java
@@ -1,4 +1,42 @@
 package com.example.demo.dto;
 
 public class JourneyDto {
+
+    private int time;
+    private int familiarity;
+    private boolean interactive;
+    private String[] themes;
+
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getFamiliarity() {
+        return familiarity;
+    }
+
+    public void setFamiliarity(int familiarity) {
+        this.familiarity = familiarity;
+    }
+
+    public boolean isInteractive() {
+        return interactive;
+    }
+
+    public void setInteractive(boolean interactive) {
+        this.interactive = interactive;
+    }
+
+    public String[] getThemes() {
+        return themes;
+    }
+
+    public void setThemes(String[] themes) {
+        this.themes = themes;
+    }
 }
