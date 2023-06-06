9c8b8db446dc12142a280b26ba3edd1b7fa42eed
aleksanderbrorson <Aleksander.brorson@gmail.com>
2023-06-06 15:12:34 +0200

    Created front page
    Created front page with heading, subheading (paragraphs) and "start"-button. Also started basic styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import JourneyPlannerPage from "./components/pages/journeyPlanner/JourneyPlannerPage";
 import "./assets/styles/css-reset.scss";
 import "./assets/styles/app.scss";
+import FrontPage from "./components/pages/frontPage/FrontPage";
 
 function App() {
   return (
     <div className="App">
-      <JourneyPlannerPage />
+      {/* <JourneyPlannerPage /> */}
+      <FrontPage />
     </div>
   );
 }
