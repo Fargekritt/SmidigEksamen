08100d9ef79a34274d07e748d238550134dc8679
Amund <lullinj98@gmail.com>
2023-06-06 14:31:35 +0200

    application.properties  * Changed default schemas  * set create schemas to true V001_create_tables.sql added sql to create tables

--- backend/src/main/resources/db/migration/V001__create_tables.sql
+++ backend/src/main/resources/db/migration/V001__create_tables.sql
@@ -1,8 +1,50 @@
-CREATE TABLE temp
+CREATE TABLE artist
 (
-    id          int primary key,
-    name        varchar(100),
-    art_number  varchar(100),
-    category    varchar(100),
-    description varchar(100)
+    artist_ID   INT AUTO_INCREMENT PRIMARY KEY,
+    artist_name VARCHAR(100)
+);
+
+CREATE TABLE exhibition
+(
+    exhibition_ID   INT AUTO_INCREMENT PRIMARY KEY,
+    exhibition_name VARCHAR(100),
+    floor           INT
+);
+CREATE TABLE painting
+(
+    painting_ID   INT AUTO_INCREMENT PRIMARY KEY,
+    painting_name VARCHAR(50),
+    date_created  VARCHAR(20),
+    description   TEXT,
+    image_path    VARCHAR(250),
+    painter_ID    INT,
+    exhibition_ID INT,
+    FOREIGN KEY (painter_ID) REFERENCES artist (artist_ID),
+    FOREIGN KEY (exhibition_ID) REFERENCES exhibition (exhibition_ID)
+);
+
+CREATE TABLE comment
+(
+    comment_ID   INT AUTO_INCREMENT PRIMARY KEY,
+    nickname     VARCHAR(30),
+    comment_text VARCHAR(2000),
+    comment_date DATE,
+    likes        INT,
+    painting_ID  INT,
+    FOREIGN KEY (painting_ID) REFERENCES painting (painting_ID)
+);
+
+
+CREATE TABLE theme
+(
+    theme_ID   INT AUTO_INCREMENT PRIMARY KEY,
+    theme_name VARCHAR(50)
+);
+
+CREATE TABLE painting_theme
+(
+    painting_ID INT,
+    theme_ID    INT,
+    FOREIGN KEY (painting_ID) REFERENCES painting (painting_ID),
+    FOREIGN KEY (theme_ID) REFERENCES theme (theme_ID)
 );
