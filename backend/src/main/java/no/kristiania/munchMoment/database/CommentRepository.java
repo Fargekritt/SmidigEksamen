95a8a8ef78fc4a47b4b94d8c49dcb5bcc6c59e74
Amund <lullinj98@gmail.com>
2023-06-07 14:04:18 +0200

    Updated CommentController.java to use CommentRepository and return 404 when not finding comment

--- backend/src/main/java/no/kristiania/munchMoment/database/CommentRepository.java
+++ backend/src/main/java/no/kristiania/munchMoment/database/CommentRepository.java
@@ -3,5 +3,5 @@ package no.kristiania.munchMoment.database;
 import no.kristiania.munchMoment.entities.Comment;
 import org.springframework.data.repository.ListCrudRepository;
 
-public interface CommentRepository extends ListCrudRepository<Comment,Integer> {
+public interface CommentRepository extends ListCrudRepository<Comment,Long> {
 }
