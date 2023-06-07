849c1fa7ccfba5c289fbb8747ed6126825ed93f6
RikaTob <rikatob@hotmail.com>
2023-06-07 16:47:45 +0200

    JourneyBuilder * Updated exhibitionID to correct value.

--- backend/src/main/java/no/kristiania/munchMoment/entities/journey/JourneyBuilder.java
+++ backend/src/main/java/no/kristiania/munchMoment/entities/journey/JourneyBuilder.java
@@ -53,6 +53,7 @@ public class JourneyBuilder {
     /**
      * Builds a journey with the correct stops determined by the field values.
      * Add stops to the journey for each field, differentiates on witch and quantity by a certain field value.
+     *
      * @return Journey
      */
     public Journey build() {
@@ -60,38 +61,38 @@ public class JourneyBuilder {
 
         // 0-180
         if (time < 90) {
-            journey.addStop(1,4);
+            journey.addStop(1, 4);
         } else {
-            journey.addStop(2,4);
-            journey.addStop(3,4);
-            journey.addStop(4,3);
-            journey.addStop(17,0);
+            journey.addStop(2, 4);
+            journey.addStop(3, 4);
+            journey.addStop(4, 3);
+            journey.addStop(17, 3);
         }
 
         // 0-4
         if (familiarity < 2) {
-            journey.addStop(5,3);
-            journey.addStop(6,3);
+            journey.addStop(5, 3);
+            journey.addStop(6, 3);
         } else {
-            journey.addStop(7,3);
-            journey.addStop(8,3);
+            journey.addStop(7, 3);
+            journey.addStop(8, 3);
         }
 
         // Landscape , Interior, Portrait, Place, Nature, Love, Time, Family, Anxiety,Jealousy
         if (themes.contains("Landscape") || themes.contains("Interior") || themes.contains("Portrait") || themes.contains("Place") || themes.contains("Nature")) {
-            journey.addStop(9,6);
-            journey.addStop(10,3);
+            journey.addStop(9, 6);
+            journey.addStop(10, 3);
         } else {
-            journey.addStop(11,0);
-            journey.addStop(12,0);
+            journey.addStop(11, 3);
+            journey.addStop(12, 3);
         }
 
         if (interactive) {
-            journey.addStop(13,0);
-            journey.addStop(14,0);
+            journey.addStop(13, 3);
+            journey.addStop(14, 3);
         } else {
-            journey.addStop(15,0);
-            journey.addStop(16,0);
+            journey.addStop(15, 3);
+            journey.addStop(16, 3);
         }
 
         return journey;
