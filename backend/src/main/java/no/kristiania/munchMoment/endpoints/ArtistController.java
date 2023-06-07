5ee6f2f71331d90b3ef57772b2cdcd560b411229
Amund <lullinj98@gmail.com>
2023-06-07 13:51:29 +0200

    Deleted all old daos
    Exhibition.java
     * Made into a entity
    
    ArtistController.java
     * /api/artist/{id} now gives 404 if artist is not found. and 200 if it is found

--- backend/src/main/java/no/kristiania/munchMoment/endpoints/ArtistController.java
+++ backend/src/main/java/no/kristiania/munchMoment/endpoints/ArtistController.java
@@ -1,16 +1,32 @@
 package no.kristiania.munchMoment.endpoints;
 
+import no.kristiania.munchMoment.database.ArtistRepository;
+import no.kristiania.munchMoment.entities.Artist;
 import no.kristiania.munchMoment.entities.Painting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8080"})
 @RequestMapping("/api/artist")
 public class ArtistController {
 
+    @Autowired
+    ArtistRepository artistRepository;
+
     @GetMapping("/{id}")
-    public Artist getArtist(@PathVariable long id) {
-        return new Artist();
+    public ResponseEntity<Artist> getArtist(@PathVariable int id) {
+        Optional<Artist> optionalArtist = artistRepository.findById(id);
+        return optionalArtist
+                .map(artist -> ResponseEntity.status(HttpStatus.OK).body(artist))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
     }
 
 
--- backend/src/main/java/no/kristiania/munchMoment/entities/Exhibition.java
+++ backend/src/main/java/no/kristiania/munchMoment/entities/Exhibition.java
@@ -1,7 +1,17 @@
 package no.kristiania.munchMoment.entities;
 
-public class Exhibition {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+import java.io.Serializable;
+
+@Entity
+public class Exhibition implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String exhibitionName;
