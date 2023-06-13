9304e930a47a61509d89567d7fc0ada526763e09
Amund <lullinj98@gmail.com>
2023-06-13 11:56:30 +0200

    Fixed indenting

--- frontend/src/services/ApiService.jsx
+++ frontend/src/services/ApiService.jsx
@@ -1,46 +1,46 @@
 import axios from "axios";
 
 const axiosConfig = {
-    baseURL: `https://munchmoment-win.azurewebsites.net/api/`,
-    baseDevURL: 'http://localhost:8080/api/'
+  baseURL: `https://munchmoment-win.azurewebsites.net/api/`,
+  baseDevURL: 'http://localhost:8080/api/'
 };
 
 function getApiUrl() {
-    if (window.location.hostname === "munchmoment-win.azurewebsites.net") {
-        return axiosConfig.baseURL;
-    } else {
-        return axiosConfig.baseDevURL;
-    }
+  if (window.location.hostname === "munchmoment-win.azurewebsites.net") {
+    return axiosConfig.baseURL;
+  } else {
+    return axiosConfig.baseDevURL;
+  }
 }
 
 const ApiService = (() => {
 
-    let baseURL = getApiUrl();
-    const postFormData = async (data, endpoint) => {
-        try {
-            return await axios.post(`${baseURL}${endpoint}`, data);
-        } catch (err) {
-            return Promise.reject(err);
-        }
-    };
+  let baseURL = getApiUrl();
+  const postFormData = async (data, endpoint) => {
+    try {
+      return await axios.post(`${baseURL}${endpoint}`, data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  };
 
-    const getById = async (endpoint, id) => {
-        try {
-            return await axios.get(`${baseURL}${endpoint}/${id}`);
-        } catch (err) {
-            return Promise.reject(err);
-        }
-    };
+  const getById = async (endpoint, id) => {
+    try {
+      return await axios.get(`${baseURL}${endpoint}/${id}`);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  };
 
-    const getAll = async endpoint => {
-        try {
-            return await axios.get(`${baseURL}${endpoint}/`);
-        } catch (err) {
-            return Promise.reject(err);
-        }
-    };
+  const getAll = async endpoint => {
+    try {
+      return await axios.get(`${baseURL}${endpoint}/`);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  };
 
-    return {postFormData, getById, getAll};
+  return {postFormData, getById, getAll};
 })();
 
 export default ApiService;
