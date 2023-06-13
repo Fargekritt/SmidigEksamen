ce2fe480efe6ca50a6c1c26826c99cddfdace438
ariavalenci <aria@valenci.no>
2023-06-13 19:52:23 +0200

    Added basic styling and fixed paintingId for form submit

--- frontend/src/components/pages/comments/CommentPage.jsx
+++ frontend/src/components/pages/comments/CommentPage.jsx
@@ -1,8 +1,6 @@
-import { useState, useEffect, useLayoutEffect, useRef } from "react";
+import { useState, useRef } from "react";
 import CommentItem from "../painting/CommentItem";
-import { render } from "@testing-library/react";
 import ApiService from "../../../services/ApiService";
-// import "../painting/painting-page.scss";
 import sendIcon from "../../../assets/icons/paper-plane.svg";
 import arrowDown from "../../../assets/icons/arrow-down.svg";
 import "./comment-page.scss";
@@ -16,6 +14,7 @@ const CommentPage = ({
 }) => {
   const inputRef = useRef();
   const [textareaHasLength, setTextareaHasLength] = useState(false);
+  const [animateClosePage, setAnimateClosePage] = useState(false);
   const renderComments = comments.map((comment, i) => {
     return (
       <CommentItem key={comment.id} text={comment.text} date={comment.date} />
@@ -27,9 +26,9 @@ const CommentPage = ({
 
     const dataToSubmit = {
       text: Object.values(e.target)[0].value,
-      paintingId: 3,
+      paintingId: paintingId,
     };
-    console.log(typeof dataToSubmit);
+
     submitData(dataToSubmit);
   };
 
@@ -44,38 +43,49 @@ const CommentPage = ({
       console.log(err);
     }
   };
+  console.log(isVisible);
 
   return (
     <div
+      onAnimationEnd={() => {
+        if (animateClosePage === true) {
+          setIsVisible(false);
+        }
+      }}
       className={`page comment-page ${
-        isVisible !== undefined && isVisible ? "visible" : "not-visible"
+        !animateClosePage ? "visible" : "not-visible"
       }`}
     >
       <header className="header">
-        <button className="back-button" onClick={() => setIsVisible(false)}>
+        <button
+          className="back-button"
+          onClick={() => setAnimateClosePage(true)}
+        >
           <img src={arrowDown} alt="left pointing icon" />
         </button>
       </header>
-      <div className="comment-list">{renderComments}</div>
-      <form className="comment-form" onSubmit={handleSubmit}>
-        <textarea
-          ref={inputRef}
-          className="comment-input"
-          type="text"
-          onInput={e => {
-            if (e.currentTarget.value.length > 0) {
-              setTextareaHasLength(true);
-            } else {
-              setTextareaHasLength(false);
-            }
-          }}
-        />
-        {textareaHasLength && (
-          <button className="submit-comment-button">
-            <img src={sendIcon} alt="paper plane icon"></img>
-          </button>
-        )}
-      </form>
+      <div className="comment-content-wrapper">
+        <div className="comment-list">{renderComments}</div>
+        <form className="comment-form" onSubmit={handleSubmit}>
+          <textarea
+            ref={inputRef}
+            className="comment-input"
+            type="text"
+            onInput={e => {
+              if (e.currentTarget.value.length > 0) {
+                setTextareaHasLength(true);
+              } else {
+                setTextareaHasLength(false);
+              }
+            }}
+          />
+          {textareaHasLength && (
+            <button className="submit-comment-button">
+              <img src={sendIcon} alt="paper plane icon"></img>
+            </button>
+          )}
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/src/components/pages/painting/CommentItem.jsx
+++ frontend/src/components/pages/painting/CommentItem.jsx
@@ -1,8 +1,16 @@
 const CommentItem = ({ text, date }) => {
+  const dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  const dateString = new Date(date).toLocaleDateString("nb-NO", dateOptions);
+
   return (
     <div className="comment">
       <p className="text">{text}</p>
-      <p className="date">{date}</p>
+      <p className="date">{dateString}</p>
     </div>
   );
 };
