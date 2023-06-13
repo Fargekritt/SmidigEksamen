1dfd1abb59c51a4c7f1d3048d6a1e84161f7959f
Amund <lullinj98@gmail.com>
2023-06-13 11:23:19 +0200

    Fixed filter

--- backend/src/main/java/no/kristiania/munchMoment/Components/RedirectToIndexFilter.java
+++ backend/src/main/java/no/kristiania/munchMoment/Components/RedirectToIndexFilter.java
@@ -1,29 +1,32 @@
-package no.kristiania.munchMoment;
+package no.kristiania.munchMoment.Components;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 public class RedirectToIndexFilter implements Filter {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         String requestURI = req.getRequestURI();
+        if (logger.isDebugEnabled()) logger.debug(requestURI);
+
 
         if (requestURI.startsWith("/api")) {
             chain.doFilter(request, response);
             return;
         }
-
-        if (requestURI.startsWith("/*")) {
+        if (requestURI.startsWith("/static")) {
             chain.doFilter(request, response);
             return;
         }
-
-        // all requests not api or static will be forwarded to index page.
-        request.getRequestDispatcher("/").forward(request, response);
+        request.getRequestDispatcher("/index.html").forward(request, response);
     }
 }
