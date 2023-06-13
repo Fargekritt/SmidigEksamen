4e5bb932d33cb482db247cf7417064f54b50d9d1
Amund <lullinj98@gmail.com>
2023-06-13 11:53:38 +0200

    ApiService.jsx now uses localhost:8080 if hostname not == to azure

--- frontend/src/services/ApiService.jsx
+++ frontend/src/services/ApiService.jsx
@@ -1,35 +1,45 @@
 import axios from "axios";
 
 const axiosConfig = {
-  baseURL: `https://munchmoment-win.azurewebsites.net/api/`,
+    baseURL: `https://munchmoment-win.azurewebsites.net/api/`,
 };
 
+function getApiUrl() {
+    if (window.location.hostname === "munchmoment-win.azurewebsites.net") {
+        return axiosConfig.baseURL;
+    } else {
+        return "http://localhost:8080/api/";
+    }
+}
+
 const ApiService = (() => {
-  const postFormData = async (data, endpoint) => {
-    try {
-      return await axios.post(`${axiosConfig.baseURL}${endpoint}`, data);
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  };
+    // let [baseURL, setBaseURL] = useState(getApiUrl());
+    let baseURL = getApiUrl();
+    const postFormData = async (data, endpoint) => {
+        try {
+            return await axios.post(`${baseURL}${endpoint}`, data);
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    };
 
-  const getById = async (endpoint, id) => {
-    try {
-      return await axios.get(`${axiosConfig.baseURL}${endpoint}/${id}`);
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  };
+    const getById = async (endpoint, id) => {
+        try {
+            return await axios.get(`${baseURL}${endpoint}/${id}`);
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    };
 
-  const getAll = async endpoint => {
-    try {
-      return await axios.get(`${axiosConfig.baseURL}${endpoint}/`);
-    } catch (err) {
-      return Promise.reject(err);
-    }
-  };
+    const getAll = async endpoint => {
+        try {
+            return await axios.get(`${baseURL}${endpoint}/`);
+        } catch (err) {
+            return Promise.reject(err);
+        }
+    };
 
-  return { postFormData, getById, getAll };
+    return {postFormData, getById, getAll};
 })();
 
 export default ApiService;
