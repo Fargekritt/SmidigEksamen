aed3321b34f21d43a73fe7e201b591e4ca85bb81
Amund <lullinj98@gmail.com>
2023-06-14 20:45:59 +0200

    RenderImage.jsx  * onLoad    * checks aspect ratio and sets style to portrait/landscape based on the ratio  app.scss   * Created landscape/portrait styles with test values

--- frontend/src/components/shared/RenderImage.jsx
+++ frontend/src/components/shared/RenderImage.jsx
@@ -1,39 +1,55 @@
 import React, {useState} from "react";
+//Lager et Image DOM elementm usikker på om det kan brukes?
+function FocalPoint(imageUrl) {
+  const [focalPoint, setFocalPoint] = useState({x: 0, y: 0});
+  // Create a new Image object
+  let img = new Image();
 
-function FocalPoint (imageUrl){
-    const [focalPoint, setFocalPoint] = useState({ x: 0, y: 0 });
-    // Create a new Image object
-    let img = new Image();
+  // Once the image has loaded, calculate the focal point
+  img.onload = function () {
+    const aspectRatio = this.width / this.height;
+    let focalX, focalY;
 
-    // Once the image has loaded, calculate the focal point
-    img.onload = function () {
-        const aspectRatio = this.width / this.height;
-        let focalX, focalY;
-
-        if (aspectRatio > 1) {
-            // The image is landscape
-            focalX = this.width / 2;  // set the focal point to the center of the image
-            focalY = this.height / 2;
-        } else {
-            // The image is portrait
-            focalX = this.width / 2;
-            focalY = this.height / 3;  // set the focal point a third of the way down the image
-        }
-        img.x = focalX;
-        img.y = focalY;
-        setFocalPoint({ x: focalX, y: focalY });
+    if (aspectRatio > 1) {
+      // The image is landscape
+      focalX = this.width / 2;  // set the focal point to the center of the image
+      focalY = this.height / 2;
+    } else {
+      // The image is portrait
+      focalX = this.width / 2;
+      focalY = this.height / 3;  // set the focal point a third of the way down the image
     }
+    img.x = focalX;
+    img.y = focalY;
+    setFocalPoint({x: focalX, y: focalY});
+  }
 
-    // Start loading the image
-    img.src = imageUrl;
+  // Start loading the image
+  img.src = imageUrl;
+  img.toString()
 }
-const RenderImage = ({ image, altText }) => {
+
+const RenderImage = ({image, altText}) => {
   return (
     <div className="image-wrapper">
+      <p className={"portrait"}>Kakemann</p>
       <img
         src={image || "./images/fallback-image.svg"}
         alt={altText}
-        onError={({ currentTarget }) => {
+        onLoad={(currentTarget) => {
+
+          let aspectRatio = currentTarget.currentTarget.naturalWidth / currentTarget.currentTarget.naturalHeight;
+          console.log("Aspect ratio: " + aspectRatio);
+          console.log(currentTarget.currentTarget.alt)
+          if (aspectRatio > 1) {//landscape
+            console.log("setting style to: landscape")
+            currentTarget.currentTarget.className = "landscape"
+          } else {
+            console.log("setting style to: portrait")
+            currentTarget.currentTarget.className = "portrait"
+          }
+        }}
+        onError={({currentTarget}) => {
           console.log("error");
           currentTarget.onerror = null;
           currentTarget.src = "./images/fallback-image.svg";
@@ -42,5 +58,6 @@ const RenderImage = ({ image, altText }) => {
     </div>
   );
 };
+
 
 export default RenderImage;
