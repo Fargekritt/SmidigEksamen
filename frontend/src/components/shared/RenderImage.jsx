6ff934188f549db1999d2e5a4b6d8b9ba2d0b133
AnnHelenGranholt <ann_helen@hotmail.no>
2023-06-14 15:38:19 +0200

    Not fully working Focal point selector

--- frontend/src/components/shared/RenderImage.jsx
+++ frontend/src/components/shared/RenderImage.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
+function FocalPoint (imageUrl){
+    const [focalPoint, setFocalPoint] = useState({ x: 0, y: 0 });
+    // Create a new Image object
+    let img = new Image();
+
+    // Once the image has loaded, calculate the focal point
+    img.onload = function () {
+        const aspectRatio = this.width / this.height;
+        let focalX, focalY;
+
+        if (aspectRatio > 1) {
+            // The image is landscape
+            focalX = this.width / 2;  // set the focal point to the center of the image
+            focalY = this.height / 2;
+        } else {
+            // The image is portrait
+            focalX = this.width / 2;
+            focalY = this.height / 3;  // set the focal point a third of the way down the image
+        }
+        img.x = focalX;
+        img.y = focalY;
+        setFocalPoint({ x: focalX, y: focalY });
+    }
+
+    // Start loading the image
+    img.src = imageUrl;
+}
 const RenderImage = ({ image, altText }) => {
   return (
     <div className="image-wrapper">
