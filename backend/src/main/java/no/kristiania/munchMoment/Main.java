64b3c400a883621ebe44a2ba7c996b142abcd8fa
Amund <lullinj98@gmail.com>
2023-06-15 19:18:50 +0200

    Clean up

--- backend/src/main/java/no/kristiania/munchMoment/Main.java
+++ backend/src/main/java/no/kristiania/munchMoment/Main.java
@@ -13,7 +13,6 @@ public class Main {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        logger.info("Starting!?");
         SpringApplication.run(Main.class, args);
         logger.info("Started!");
     }
